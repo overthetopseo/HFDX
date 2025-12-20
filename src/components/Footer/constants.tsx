7a1d908590ce965b7f70c2ce90a64882bd1b385e
overthetopseo <guy@overthetopseo.com>
2025-12-20 04:13:26 +0400

    Update HFDX branding: horizontal logo, @HFDX socials, cleanup footer

--- src/components/Footer/constants.tsx
+++ src/components/Footer/constants.tsx
@@ -2,8 +2,6 @@ import { t } from "@lingui/macro";
 import { ReactNode } from "react";
 
 import DiscordIcon from "img/ic_discord.svg?react";
-import GithubIcon from "img/ic_github.svg?react";
-import SubstackIcon from "img/ic_substack.svg?react";
 import TelegramIcon from "img/ic_telegram.svg?react";
 import XIcon from "img/ic_x.svg?react";
 
@@ -21,33 +19,11 @@ type SocialLink = {
 };
 
 export const getFooterLinks = (): Link[] => [
-  {
-    label: (
-      <>
-        <V1Icon />
-        {t`V1 Interface`}
-      </>
-    ),
-    link: "https://v1.app.gmx.io/#/v1",
-    external: true,
-  },
-  { label: t`Media Kit`, link: "https://docs.gmx.io/docs/community/media-kit", external: true },
   { label: t`Charts by TradingView`, link: "https://www.tradingview.com/", external: true },
-  // { label: "Jobs", link: "/jobs" },
 ];
 
 export const SOCIAL_LINKS: SocialLink[] = [
-  { link: "https://twitter.com/GMX_IO", name: "Twitter", icon: <XIcon className="size-16" /> },
-  { link: "https://gmxio.substack.com/", name: "Substack", icon: <SubstackIcon className="size-16" /> },
-  { link: "https://github.com/gmx-io", name: "Github", icon: <GithubIcon className="size-16" /> },
-  { link: "https://t.me/GMX_IO", name: "Telegram", icon: <TelegramIcon className="size-16" /> },
-  { link: "https://discord.gg/H5PeQru3Aa", name: "Discord", icon: <DiscordIcon className="size-16" /> },
+  { link: "https://twitter.com/HFDX", name: "Twitter", icon: <XIcon className="size-16" /> },
+  { link: "https://t.me/HFDX", name: "Telegram", icon: <TelegramIcon className="size-16" /> },
+  { link: "https://discord.gg/HFDX", name: "Discord", icon: <DiscordIcon className="size-16" /> },
 ];
-
-function V1Icon() {
-  return (
-    <div className="rounded-2 bg-typography-secondary px-3 py-2 text-[8px] font-medium text-white dark:text-black">
-      V1
-    </div>
-  );
-}
--- src/components/Seo/SEO.tsx
+++ src/components/Seo/SEO.tsx
@@ -22,7 +22,7 @@ function SEO(props) {
         <meta property="og:title" content={meta.title} />
         <meta property="og:image" content={meta.image} />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@hfdx_xyz" />
+        <meta name="twitter:site" content="@HFDX" />
         <meta name="twitter:title" content={meta.title} />
         <meta name="twitter:description" content={meta.description} />
         <meta name="twitter:image" content={meta.image} />
