45279fd5cf46b4078307646548ae4bac5c802097
overthetopseo <guy@overthetopseo.com>
2026-02-04 20:54:24 +0400

    Remove HFDX token card from stats page
    - Remove entire "Tokens" section with HFDX card
    - Remove unused GmxCard/HfdxCard component
    - Clean up unused imports for price/supply data

--- src/pages/Dashboard/DashboardV2.tsx
+++ src/pages/Dashboard/DashboardV2.tsx
@@ -3,12 +3,7 @@ import { Link } from "react-router-dom";
 
 import { ARBITRUM, AVALANCHE } from "config/chains";
 import { SyntheticsStateContextProvider } from "context/SyntheticsStateContext/SyntheticsStateContextProvider";
-import { useGmxPrice, useTotalGmxInLiquidity, useTotalGmxSupply } from "domain/legacy";
-import { useChainId } from "lib/chains";
-import { GMX_DECIMALS, getPageTitle } from "lib/legacy";
-import { expandDecimals } from "lib/numbers";
-import useWallet from "lib/wallets/useWallet";
-import { bigMath } from "sdk/utils/bigmath";
+import { getPageTitle } from "lib/legacy";
 
 import AppPageLayout from "components/AppPageLayout/AppPageLayout";
 import { ChainContentHeader } from "components/ChainContentHeader/ChainContentHeader";
@@ -18,7 +13,6 @@ import SEO from "components/Seo/SEO";
 
 import V2Icon from "img/ic_v2.svg?react";
 
-import { GmxCard } from "./HfdxCard";
 import { OverviewCard } from "./OverviewCard";
 import { StatsCard } from "./StatsCard";
 import { useDashboardChainStatsMulticall } from "./useDashboardChainStatsMulticall";
@@ -28,26 +22,8 @@ import "./DashboardV2.css";
 export const ACTIVE_CHAIN_IDS = [ARBITRUM, AVALANCHE];
 
 export default function DashboardV2() {
-  const { active, signer } = useWallet();
-  const { chainId } = useChainId();
-
-  let { total: totalGmxSupply } = useTotalGmxSupply();
-
   const statsArbitrum = useDashboardChainStatsMulticall(ARBITRUM);
   const statsAvalanche = useDashboardChainStatsMulticall(AVALANCHE);
-
-  const { gmxPrice, gmxPriceFromArbitrum, gmxPriceFromAvalanche } = useGmxPrice(
-    chainId,
-    { arbitrum: chainId === ARBITRUM ? signer : undefined },
-    active
-  );
-
-  let gmxMarketCap =
-    gmxPrice !== undefined && totalGmxSupply !== undefined
-      ? bigMath.mulDiv(gmxPrice, totalGmxSupply, expandDecimals(1, GMX_DECIMALS))
-      : undefined;
-
-  let { total: totalGmxInLiquidity } = useTotalGmxInLiquidity();
 
   return (
     <SEO title={getPageTitle(t`Stats`)}>
@@ -72,24 +48,10 @@ export default function DashboardV2() {
               <OverviewCard statsArbitrum={statsArbitrum} statsAvalanche={statsAvalanche} />
               <StatsCard />
             </div>
-            <h2 className="text-h2 px-12 font-medium">{t`Tokens`}</h2>
-            <div className="DashboardV2-token-cards">
-              <div className="stats-wrapper stats-wrapper--gmx">
-                <GmxCard
-                  chainId={chainId}
-                  gmxPrice={gmxPrice}
-                  gmxPriceFromArbitrum={gmxPriceFromArbitrum}
-                  gmxPriceFromAvalanche={gmxPriceFromAvalanche}
-                  totalGmxSupply={totalGmxSupply}
-                  gmxMarketCap={gmxMarketCap}
-                  totalGmxInLiquidity={totalGmxInLiquidity}
-                />
-              </div>
 
-              <SyntheticsStateContextProvider skipLocalReferralCode={false} pageType="pools">
-                <MarketsList />
-              </SyntheticsStateContextProvider>
-            </div>
+            <SyntheticsStateContextProvider skipLocalReferralCode={false} pageType="pools">
+              <MarketsList />
+            </SyntheticsStateContextProvider>
           </div>
         </div>
       </AppPageLayout>
